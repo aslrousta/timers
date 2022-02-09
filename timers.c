a1607ace8c73aaeffd92c35bf8487d268add3772
Ali AslRousta <aslrousta@gmail.com>
2022-02-10 00:41:21 +0330

    loop

--- timers.c
+++ timers.c
@@ -20,10 +20,56 @@
 #include "timers.h"
 
 #include <pthread.h>
+#include <time.h>
 
-int timers_init(void) { return 0; }
+static pthread_t th;
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static volatile int running = 0;
 
-int timers_cleanup(void) { return 0; }
+typedef struct ticker {
+  long long next; /* next tick in ms since era */
+  int interval;   /* interval in ms */
+  void (*callback)(void *);
+  void *arg;
+  int rep;
+} ticker_t;
+
+static void *_loop(void *arg) {
+  struct timespec era;
+  clock_gettime(CLOCK_REALTIME, &era);
+  while (1) {
+    pthread_mutex_lock(&lock);
+    if (!running) {
+      pthread_mutex_unlock(&lock);
+      break;
+    }
+    pthread_mutex_unlock(&lock);
+  }
+  return NULL;
+}
+
+int timers_init(void) {
+  int res = 0;
+  pthread_mutex_lock(&lock);
+  if (pthread_create(&th, NULL, _loop, NULL) < 0) {
+    res = -1;
+  }
+end:
+  pthread_mutex_unlock(&lock);
+  return res;
+}
+
+int timers_cleanup(void) {
+  pthread_mutex_lock(&lock);
+  if (!running) {
+    pthread_mutex_unlock(&lock);
+    return 0;
+  }
+  running = 0;
+  pthread_mutex_unlock(&lock);
+  pthread_join(th, NULL);
+  return 0;
+}
 
 int timers_start(int ms, void (*cb)(void *), void *v, int rep, int *id) {
   return 0;
